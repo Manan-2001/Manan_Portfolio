c6ea8a17e96dc1d8d4ef6937f8f4a0eae62f135e
Manan-2001 <jainmanan03052001@gmail.com>
2024-07-17 20:06:53 +0530

    Project info  work done videos pending

--- src/app/sport-activity-recognization/sport-activity-recognization.component.ts
+++ src/app/sport-activity-recognization/sport-activity-recognization.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +10,33 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './sport-activity-recognization.component.scss'
 })
 export class SportActivityRecognizationComponent {
+  private observer!: IntersectionObserver;
 
+  constructor(private el: ElementRef, private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const contents = this.el.nativeElement.querySelectorAll('.content');
+
+      this.observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.renderer.addClass(entry.target, 'float-right-to-left');
+          }
+        });
+      }, {
+        threshold: 0.1
+      });
+
+      contents.forEach((content: Element) => {
+        this.observer.observe(content);
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
 }
